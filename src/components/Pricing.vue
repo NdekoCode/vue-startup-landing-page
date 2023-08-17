f2d4132877ff3cb8811c03687befcaa896fae3de
NdekoCode <arickbulakali@gmail.com>
2023-08-17 08:53:35 +0200

    refactor: add button play to testimonial

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -1,4 +1,13 @@
-<script lang="ts" setup></script>
+<script setup lang="ts"></script>
 <template>
-  <section class="py-20 bg-white">Pricing</section>
+  <section class="py-10">
+    <div class="container max-w-6xl flex-col-center">
+      <div class="max-w-xl mx-auto mb-10 text-center">
+        <h2 class="mb-10 text-3xl">Make the wise decision for your business</h2>
+        <p class="text-sm text-gray-600">Choose from our affordable 3 packages</p>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped></style>
--- src/components/TestimonialItem.vue
+++ src/components/TestimonialItem.vue
@@ -13,6 +13,17 @@ const { testimonial } = defineProps({
   <article class="max-w-sm">
     <p class="max-w-xs mb-8 text-sm text-gray-700">{{ testimonial.content }}</p>
     <div class="relative mb-3">
+      <button
+        class="absolute w-10 h-10 text-gray-700 -translate-x-1/2 -translate-y-1/2 bg-white rounded-full shadow-lg animate-pulse top-1/2 left-1/2 flex-center"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3" viewBox="0 0 20 20">
+          <path
+            fill-rule="evenodd"
+            fill="currentColor"
+            d="M16.75 10.83L4.55 19A1 1 0 0 1 3 18.13V1.87A1 1 0 0 1 4.55 1l12.2 8.13a1 1 0 0 1 0 1.7z"
+          />
+        </svg>
+      </button>
       <img
         :src="testimonial.image"
         :alt="testimonial.userName"
