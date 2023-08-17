501087d57929af9bd3721ab2eabb55fd2c3088bd
NdekoCode <arickbulakali@gmail.com>
2023-08-17 09:45:01 +0200

    feat: add footer structure

--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -1,4 +1,18 @@
 <script lang="ts" setup></script>
 <template>
-  <header>Main Footer</header>
+  <footer class="py-10 bg-[#181818]">
+    <div class="container">
+      <div class="p-10 rounded-md mb-20 flex-between-center bg-[#d1edf5]">
+        <h2 class="max-w-md text-3xl">Grow your business fast with Sark Sass Landing.</h2>
+        <a
+          href="#"
+          class="inline-block px-5 py-3 mb-5 text-sm text-center text-white transition-colors duration-300 bg-gray-900 border border-transparent rounded-lg md:px-8 lg:px-10"
+          >Download Sark Now</a
+        >
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <ul></ul>
+      </div>
+    </div>
+  </footer>
 </template>
